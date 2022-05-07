7a6e5ccc1eecb4c8c7d8d4496dcde9ebcfa71523
A.R.Z <lexz.zaldivar@gmail.com>
2022-05-07 03:38:49 -0400

    Add GTAG

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
+import Script from 'next/script'
 import '../styles/main.scss'
 
+const GID = 'G-0X2VKGWSPN';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script strategy='afterInteractive' src={`https://www.googletagmanager.com/gtag/js?id=${GID}`}/>
+      <Script id='gtag-init'
+        strategy='afterInteractive' 
+        dangerouslySetInnerHTML={{
+        __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', '${GID}');
+      `
+      }} />
+    </>
+  )
 }
 
 export default MyApp
